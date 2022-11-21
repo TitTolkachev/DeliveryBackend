1dc161abbe956588d789d7dddd05a717b203869c
Тит Game <tit.tolk07022003@gmail.com>
2022-11-21 17:47:33 +0700

    Рабочее тестовое приложение по мануалу

---
+++ DeliveryBackend/Data/Models/WeatherForecast.cs
@@ -0,0 +1,9 @@
+﻿namespace DeliveryBackend.Data.Models;
+
+public class WeatherForecast
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public int TemperatureC { get; set; }
+    public string? Summary { get; set; }
+}
---
+++ DeliveryBackend/Services/IWeatherService.cs
@@ -0,0 +1,41 @@
+﻿using DeliveryBackend.Data;
+using DeliveryBackend.Data.Models;
+using DeliveryBackend.Data.Models.DTO;
+
+namespace DeliveryBackend.Services;
+public interface IWeatherService
+{
+    WeatherForecastDto[] GenerateWeather();
+    Task Add(WeatherForecastDto model);
+}
+
+public class WeatherService : IWeatherService
+{
+    private readonly ApplicationDbContext _context;
+
+    public WeatherService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public WeatherForecastDto[] GenerateWeather()
+    {
+        return _context.WeatherForecasts.Select(x => new WeatherForecastDto
+        {
+            Date = x.Date,
+            Summary = x.Summary,
+            TemperatureC = x.TemperatureC
+        }).ToArray();
+    }
+
+    public async Task Add(WeatherForecastDto model)
+    {
+        await _context.WeatherForecasts.AddAsync(new WeatherForecast
+        {
+            Date = model.Date,
+            Summary = model.Summary,
+            TemperatureC = model.TemperatureC
+        });
+        await _context.SaveChangesAsync();
+    }
+}
