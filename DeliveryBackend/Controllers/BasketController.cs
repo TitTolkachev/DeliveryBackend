a697f02c6241c17b4cfd56655f012018a28b69a5
Тит Game <tit.tolk07022003@gmail.com>
2022-11-22 22:19:38 +0700

    Создал пустые контроллеры Попробовал сделать BasketController, вроде +- норм

---
+++ DeliveryBackend/Controllers/BasketController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryBackend.Controllers;
+
+[ApiController]
+[Route("api/basket")]
+public class BasketController : ControllerBase
+{
+    // private readonly IBasketService _basketService;
+    //
+    // public BasketController(IBasketService basketService)
+    // {
+    //     _basketService = basketService;
+    // }
+    //
+    // [HttpGet]
+    // [Authorize]
+    // [Authorize(Policy = "ValidateToken")]
+    // public async Task<DishBasketDto> GetUserCart()
+    // {
+    //     return await _basketService.GetUserCart();
+    // }
+    //
+    // [HttpPost]
+    // [Authorize]
+    // [Authorize(Policy = "ValidateToken")]
+    // [Route("dish/{dishId}")]
+    // public async Task AddDishToCart(Guid dishId)
+    // {
+    //     await _basketService.AddDishToCart(dishId);
+    // }
+    //
+    // [HttpDelete]
+    // [Authorize]
+    // [Authorize(Policy = "ValidateToken")]
+    // [Route("dish/{dishId}")]
+    // public async Task DecreaseDishQuantityInCart(Guid dishId)
+    // {
+    //     await _basketService.AddDishToCart(dishId);
+    // }
+}
---
+++ DeliveryBackend/Controllers/DishController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryBackend.Controllers;
+
+[ApiController]
+[Route("api/dish")]
+public class DishController : ControllerBase
+{
+    
+}
