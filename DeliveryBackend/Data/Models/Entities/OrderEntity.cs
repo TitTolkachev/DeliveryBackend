670224f26973a1edc3c4b0edcc4b15ca79694e9d
Тит Game <tit.tolk07022003@gmail.com>
2022-11-23 13:29:51 +0700

    Добавил сущности заказ, блюдо, пользователь

---
+++ DeliveryBackend/Data/Models/Entities/OrderEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DeliveryBackend.Data.Models.Enums;
+
+namespace DeliveryBackend.Data.Models.Entities;
+
+public class OrderEntity
+{
+    public Guid Id { get; set; }
+    [Required]
+    public DateTime DeliveryTime { get; set; }
+    [Required]
+    public DateTime OrderTime { get; set; }
+    [Required]
+    public OrderStatus Status { get; set; }
+    [Required]
+    public double Price { get; set; }
+    [Required]
+    public List<DishEntity?> Dishes { get; set; }
+    [Required]
+    [MinLength(1)]
+    public string Address { get; set; }
+    [Required]
+    public UserEntity User { get; set; }
+}
---
+++ DeliveryBackend/Data/Models/Entities/UserEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DeliveryBackend.Data.Models.Enums;
+
+namespace DeliveryBackend.Data.Models.Entities;
+
+public class UserEntity
+{
+    public Guid Id { get; set; }
+    [Required]
+    [MinLength(1)]
+    public string FullName { get; set; }
+    public DateTime BirthDate { get; set; }
+    [Required]
+    public Gender Gender { get; set; }
+    public string PhoneNumber { get; set; }
+    [Required]
+    [MinLength(1)]
+    [RegularExpression(@"[a-zA-Z]+\w*@[a-zA-Z]+\.[a-zA-Z]+")]
+    public string Email { get; set; }
+    public string Address { get; set; }
+    [Required]
+    [MinLength(6)]
+    public string Password { get; set; }
+}
