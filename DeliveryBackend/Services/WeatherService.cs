6c7b86628f09cc1ab15199a1e85a771c4267f3dc
Тит Game <tit.tolk07022003@gmail.com>
2022-11-30 22:03:15 +0700

    Разбил IWeatherService, потом его вообще удалю

---
+++ DeliveryBackend/Services/WeatherService.cs
@@ -0,0 +1,36 @@
+﻿using DeliveryBackend.Data;
+using DeliveryBackend.Data.Models;
+using DeliveryBackend.Data.Models.DTO;
+
+namespace DeliveryBackend.Services;
+
+public class WeatherService : IWeatherService
+{
+    private readonly ApplicationDbContext _context;
+
+    public WeatherService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public WeatherForecastDto[] GenerateWeather()
+    {
+        return _context.WeatherForecasts.Select(x => new WeatherForecastDto
+        {
+            Date = x.Date,
+            Summary = x.Summary,
+            TemperatureC = x.TemperatureC
+        }).ToArray();
+    }
+
+    public async Task Add(WeatherForecastDto model)
+    {
+        await _context.WeatherForecasts.AddAsync(new WeatherForecast
+        {
+            Date = model.Date,
+            Summary = model.Summary,
+            TemperatureC = model.TemperatureC
+        });
+        await _context.SaveChangesAsync();
+    }
+}
